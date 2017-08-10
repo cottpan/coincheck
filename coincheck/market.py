cf80fe36127e1d6acfe30488a3e4402f6e175d1c
cottpan <lok.mem@gmail.com>
2017-08-10 18:17:52 +0900

    Adding rate

--- coincheck/market.py
+++ coincheck/market.py
@@ -8,7 +8,20 @@ document: https://coincheck.com/documents/exchange/api
 base_url = "https://coincheck.com"
 api_urls = { 'ticker'     : '/api/ticker',
              'trades'     : '/api/trades',
-             'order_books': '/api/order_books'
+             'order_books': '/api/order_books',
+             'rate_btc'   : '/api/rate/btc_jpy',
+             'rate_eth'   : '/api/rate/eth_jpy',
+             'rate_etc'   : '/api/rate/etc_jpy',
+             'rate_dao'   : '/api/rate/dao_jpy',
+             'rate_lsk'   : '/api/rate/lsk_jpy',
+             'rate_fct'   : '/api/rate/fct_jpy',
+             'rate_xmr'   : '/api/rate/xmr_jpy',
+             'rate_rep'   : '/api/rate/rep_jpy',
+             'rate_xrp'   : '/api/rate/xrp_jpy',
+             'rate_zec'   : '/api/rate/zec_jpy',
+             'rate_xem'   : '/api/rate/xem_jpy',
+             'rate_ltc'   : '/api/rate/ltc_jpy',
+             'rate_dash'  : '/api/rate/dash_jpy'
              }
 
 class Market(object):
@@ -23,18 +36,22 @@ class Market(object):
             return ast.literal_eval(requests.get(base_url + api_urls.get(url)).text)
         except Exception as e:
             print(e)
-    
+
     def ticker(self):
         '''get latest information of coincheck market'''
-        return self.public_api('ticker') 
-    
+        return self.public_api('ticker')
+
     def trades(self):
         '''get latest deal history of coincheck market'''
-        return self.public_api('trades') 
-    
+        return self.public_api('trades')
+
     def orderbooks(self):
         '''get latest asks/bids information of coincheck market'''
-        return self.public_api('order_books') 
+        return self.public_api('order_books')
+
+    def rate(self, currency_pair):
+        '''get latest currency rate of coincheck market'''
+        return self.public_api('rate_' + currency_pair)
 
 
 if __name__ == '__main__':
